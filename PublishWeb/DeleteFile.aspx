55a09c07b7f4775492219a63b709dcc6ca68a1db
wiki.wan <wgang10@foxmail.com>
2014-12-15 21:49:02 +0800

    no message

---
+++ PublishWeb/DeleteFile.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageZiYang.master" AutoEventWireup="true" CodeFile="DeleteFile.aspx.cs" Inherits="DeleteFile" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <asp:TextBox ID="txtFileName" runat="server" Width="300px"></asp:TextBox>
+    <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="查找" Width="80px" />
+    <asp:TextBox ID="txtFilePath" runat="server" Width="300px"></asp:TextBox>
+    <br />
+    <asp:TextBox ID="txtVerification" runat="server"></asp:TextBox>
+    <asp:Button ID="btnDelateFile" runat="server" OnClick="btnDelate_Click" Text="删除文件" Width="80px" />
+    <asp:Button ID="btnDeleteFolder" runat="server" OnClick="btnDeleteFolder_Click" Text="删除文件夹" />
+    <br />
+    <asp:Label ID="lbMessage" runat="server" ForeColor="Red" Text="Label"></asp:Label>
+
+</asp:Content>
+
---
+++ PublishWeb/DeleteFile.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DeleteFile : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        lbMessage.Text = "";
+        try
+        {
+            txtFilePath.Text = System.Web.HttpContext.Current.Server.MapPath(txtFileName.Text);
+            txtFilePath.DataBind();
+        }
+        catch (Exception ex)
+        {
+            lbMessage.Text = ex.Message;
+            lbMessage.DataBind();
+        }
+
+    }
+    protected void btnDelate_Click(object sender, EventArgs e)
+    {
+        if (txtVerification.Text != "wan") return;
+        lbMessage.Text = "";
+        try
+        {
+            File.Delete(txtFilePath.Text);
+            lbMessage.Text = "完成删除";
+        }
+        catch (Exception ex)
+        {
+            lbMessage.Text = ex.Message;
+            lbMessage.DataBind();
+        }
+    }
+    protected void btnDeleteFolder_Click(object sender, EventArgs e)
+    {
+        if (txtVerification.Text != "wan") return;
+        lbMessage.Text = "";
+        try
+        {
+            DirectoryInfo di = new DirectoryInfo(txtFilePath.Text);
+            di.Delete(true);
+            lbMessage.Text = "完成删除";
+        }
+        catch (Exception ex)
+        {
+            lbMessage.Text = ex.Message;
+            lbMessage.DataBind();
+        }
+    }
+}
